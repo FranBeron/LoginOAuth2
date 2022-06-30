1fba5c8da1f8e6d9a5a06146b04873d02e2e0152
franberon <franconahuelberongarcia@gmail.com>
2022-06-30 19:54:44 -0300

    add styles to forms

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   constructor(private auth: Auth) {}
-    register({email, password}: any) {
-      return createUserWithEmailAndPassword(this.auth, email, password);
-    }
+  register({ email, password }: any) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
 
-    login({email, password}: any) {
-      return signInWithEmailAndPassword(this.auth, email, password);
-    }
+  login({ email, password }: any) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
 
-    logout() {
-      return signOut(this.auth);
-    }
+  logout() {
+    return signOut(this.auth);
+  }
 
-    loginWithGoogle() {
-      return signInWithPopup(this.auth, new GoogleAuthProvider());
-    }
+  loginWithGoogle() {
+    return signInWithPopup(this.auth, new GoogleAuthProvider());
+  }
 }
